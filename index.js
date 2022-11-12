8405544795ad533374275aa0a104a33536ff6c22
Daniel <danielmuniz902@gmail.com>
2022-11-12 14:52:08 +0100

    Variables de entorno

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express')
+require('dotenv').config()
+
+
 
 // Crear servidor express
 
-const app = express();
+const app = express()
 
 // Rutas
-app.get('/',(req, res)=>{
 
-    res.json({
-        ok: true
-    })
-})
+// Directorio publico
+app.use( express.static('public') )
+
+// app.get('/',(req, res)=>{
+
+//     res.json({
+//         ok: true
+//     })
+// })
 
 // Escuchar peticiones
 
-app.listen(4000, ()=>{
-    console.log(`Servidor corriendo en puerto ${4000}` );
+app.listen(process.env.PORT, ()=>{
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}` );
 })
